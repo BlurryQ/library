865bbb73d921a48d749a3175dea7cef226839b9d
BlurryQ <jazz.barlow@outlook.com>
2024-05-02 21:23:04 +0100

    Improved JS functions
    Removed template books from fresh page load.
    Enabled new books to load in without prior books.
    Created percentage limit.
    Corrected page comparison on forms.
    Added read toggle - full function to be implemented.

--- script.js
+++ script.js
@@ -11,21 +11,23 @@ FORM.style.display = "none"
 
 const myLibrary = [];
 
-function Book(title, author, pages, pagesRead, finished) {
+function Book(title, author, pages, pagesRead, finished, bookIndex) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.pagesRead = pagesRead;
   this.finished = finished;
+  this.bookIndex = bookIndex;
 }
 
-function addBookToLibrary(title, author, pages, pagesRead, finished) {
-    let newBook = new Book(title, author, pages, pagesRead, finished);
+function addBookToLibrary(title, author, pages, pagesRead, finished, bookIndex) {
+    let newBook = new Book(title, author, pages, pagesRead, finished, bookIndex);
     myLibrary.push(newBook);
 }
 
-const THE_HOBBIT = addBookToLibrary("The Hobbit", "J.R.R.Tolkien", 295, 10, false),
-EMPIRE_OF_THE_VAMPIRE = addBookToLibrary("Empire of the Vampire", "Jay Kristoff", 739, 729, true);
+/* const THE_HOBBIT = addBookToLibrary("The Hobbit", "J.R.R.Tolkien", 295, 10, false),
+EMPIRE_OF_THE_VAMPIRE = addBookToLibrary("Empire of the Vampire", "Jay Kristoff", 739, 739, true);
+ */
 
 const TITLE = document.getElementById("title"),
 AUTHOR = document.getElementById("author"),
@@ -34,7 +36,9 @@ PAGES_READ = document.getElementById("pagesRead"),
 FINISHED = document.getElementById("finished"),
 SAVE = document.getElementById("save"),
 CLOSE = document.getElementById("close"),
-DISPLAY = document.getElementById("display")
+DISPLAY = document.getElementById("display"),
+NEW_BOOK = document.getElementById("add-new-book"),
+SHELF = document.getElementById("shelf");
 
 console.table(myLibrary)
 
@@ -42,7 +46,6 @@ SAVE.setAttribute("disabled", true)
 
 TITLE.addEventListener("change", () => {
     let formComplete = checkForm();
-    console.log(formComplete)
     if(formComplete) {
         SAVE.removeAttribute("disabled");
     }
@@ -50,43 +53,60 @@ TITLE.addEventListener("change", () => {
 
 AUTHOR.addEventListener("change", () => {
     let formComplete = checkForm();
-    console.log(formComplete)
     if(formComplete) {
         SAVE.removeAttribute("disabled");
     }
 })
 
 PAGES.addEventListener("change", () => {
+    let pagesCorrect = correctPagesReadEntry()
     let formComplete = checkForm();
-    console.log(formComplete)
-    if(formComplete) {
+    console.log(pagesCorrect + " Z " + formComplete)
+    if(formComplete && pagesCorrect) {
         SAVE.removeAttribute("disabled");
+    } else {
+        SAVE.setAttribute("disabled", true);
     }
 })
 
 PAGES_READ.addEventListener("change", () => {
+    let pagesCorrect = correctPagesReadEntry()
     let formComplete = checkForm();
-    console.log(formComplete)
-    if(formComplete) {
+    console.log(pagesCorrect + " Z " + formComplete)
+    if(formComplete && pagesCorrect) {
         SAVE.removeAttribute("disabled");
+    } else {
+        SAVE.setAttribute("disabled", true);
     }
 })
 
 SAVE.addEventListener("click", () => {
-    const title = TITLE.value,
+    const bookIndex = myLibrary.length,
+    title = TITLE.value,
     author = AUTHOR.value,
     pages = PAGES.value,
-    pagesRead = PAGES_READ.value,
-    finished = isBookFinished()
-    addBookToLibrary(title, author, pages, pagesRead, finished);
-    updateDisplay(myLibrary)
-    clearInputs()
+    finished = FINISHED.checked;
+    let pagesRead = PAGES_READ.value,
+    read = isBookFinished(pages, pagesRead, finished);
+/*     console.log("read 2 " + read) */
+    if(read) { pagesRead = pages }
+    addBookToLibrary(title, author, pages, pagesRead, read, bookIndex);
+    addNewBookToShelf(myLibrary, bookIndex)
+    clearFormInputs()
     FORM.style.display = "none"
 })
 
 CLOSE.addEventListener("click", () => {
     FORM.style.display = "none"
 })
+
+NEW_BOOK.addEventListener("click", () => {
+    FORM.style.display = "flex"
+})
+
+function correctPagesReadEntry() {
+    return Number(PAGES.value) >= Number(PAGES_READ.value)
+}
 
 function checkForm() {
     if(TITLE.value !== "" 
@@ -97,87 +117,142 @@ function checkForm() {
     return false
 }
 
-function isBookFinished() {
-    return PAGES_READ.value === PAGES.value ? true : false;
-}
-
-
-function updateDisplay(library) {
-    const SHELF = document.getElementById("shelf")
-
-    while(SHELF.firstChild) {
-        SHELF.removeChild(SHELF.firstChild)
-    }
-
-    newBook = document.createElement("div")
-    newBook.classList.add("new-book")
-    addNewBook = document.createElement("div")
-    addNewBook.classList.add("add-new-book")
-    add = document.createElement("div")
-    add.classList.add("add")
-    add.textContent = "+"
-    addNewBook.appendChild(add)
-    newBook.appendChild(addNewBook)
-    SHELF.appendChild(newBook)
-
-    newBook.addEventListener("click", () => {
-        console.log("I have been clicked")
-        FORM.style.display = "flex"
-    })
-
-    for(let book in library) {
-        newBook = document.createElement("div")
-        newBook.classList.add("book")
-
-        readIcon = document.createElement("div")
-        readIcon.textContent = "[_|_]"
-        newBook.appendChild(readIcon)
-
-        title = document.createElement("div")
-        title.classList.add("title")
-        title.textContent = library[book].title
-        newBook.appendChild(title)
-
-        line1 = document.createElement("div")
-        line1.classList.add("line")
-        newBook.appendChild(line1)
-
-        author = document.createElement("div")
-        author.classList.add("author")
-        author.textContent = library[book].author
-        newBook.appendChild(author)
-
-        line2 = document.createElement("div")
-        line2.classList.add("line")
-        newBook.appendChild(line2)
-
-        pagesProgress = document.createElement("div");
-        const PAGES_READ = library[book].pagesRead,
-        TOTAL_PAGES = library[book].pages;
-        pagesProgress.textContent = `${PAGES_READ} / ${TOTAL_PAGES}`
-        newBook.appendChild(pagesProgress)
-
-        line3 = document.createElement("div")
-        line3.classList.add("line")
-        newBook.appendChild(line3)
-
-        percentageComplete = document.createElement("div");
-        percentageComplete.classList.add("percentage")
-        const PERCENT_COMPLETE = (PAGES_READ / TOTAL_PAGES) * 100
-        percentageComplete.textContent = `${Math.round(PERCENT_COMPLETE)} %`
-        newBook.appendChild(percentageComplete)
-
-        SHELF.insertBefore(newBook, SHELF.firstChild)
-    }
-}
-
-function clearInputs() {
+function clearFormInputs() {
     TITLE.value = "";
     AUTHOR.value = "";
     PAGES.value = "";
     PAGES_READ.value = "";
-    FINISHED.value = "";
+    FINISHED.checked = false;
     SAVE.setAttribute("disabled", true);
+}
+
+function isBookFinished(pages, pagesRead, finished) {
+/*     console.log("pagesT = " + pages + " || pagesR = " + pagesRead)
+    console.log("read"  + finished) */
+    if(finished) { return true }
+    return pages === pagesRead ? true : false;
+}
+
+function setBookToRead(bookIndex) {
+    const thisBook = myLibrary[bookIndex]
+    return thisBook.finished = !thisBook.finished
+}
+
+function addNewBookToShelf(library, bookIndex) {
+    const book = library[bookIndex]
+    newBook = document.createElement("div")
+    newBook.classList.add("book")
+
+    readIcon = document.createElement("div")
+
+    readIcon.classList.add("reading")
+    readIcon.textContent = "[_|_]"
+    readIcon.setAttribute("id", "toggle-read")
+    readIcon.classList.add("reading")
+    console.log("BF: " + book.finished)
+    if(book.finished) {
+        readIcon.classList.add("read") 
+        readIcon.classList.remove("reading")
+    }
+    newBook.appendChild(readIcon)
+
+    title = document.createElement("div")
+    title.classList.add("title")
+    title.textContent = book.title
+    newBook.appendChild(title)
+
+    line1 = document.createElement("div")
+    line1.classList.add("line")
+    newBook.appendChild(line1)
+
+    author = document.createElement("div")
+    author.classList.add("author")
+    author.textContent = book.author
+    newBook.appendChild(author)
+
+    line2 = document.createElement("div")
+    line2.classList.add("line")
+    newBook.appendChild(line2)
+
+    pagesProgress = document.createElement("div");
+    const PAGES_READ = book.pagesRead,
+    TOTAL_PAGES = book.pages;
+    pagesProgress.textContent = `${PAGES_READ} / ${TOTAL_PAGES}`
+    newBook.appendChild(pagesProgress)
+
+    line3 = document.createElement("div")
+    line3.classList.add("line")
+    newBook.appendChild(line3)
+
+    percentageComplete = document.createElement("div");
+    percentageComplete.classList.add("percentage")
+    const PERCENT_COMPLETE = (PAGES_READ / TOTAL_PAGES) * 100
+    percentageComplete.textContent = `${Math.round(PERCENT_COMPLETE)} %`
+    newBook.appendChild(percentageComplete)
+
+    SHELF.insertBefore(newBook, SHELF.firstChild)
+
+    TOGGLE_READ = document.getElementById("toggle-read");
+    TOGGLE_READ.addEventListener("click", () => {
+        console.log("i: " + bookIndex)
+        setBookToRead(bookIndex)
+        updateBook(myLibrary, bookIndex)
+    })
+}
+
+function updateBook(library, bookIndex) {
+    const book = library[bookIndex]
+    newBook = document.createElement("div")
+    newBook.classList.add("book")
+
+    readIcon = document.createElement("div")
+
+    readIcon.classList.add("reading")
+    readIcon.textContent = "[_|_]"
+    readIcon.setAttribute("id", "toggle-read")
+    readIcon.classList.add("reading")
+    console.log("BF: " + book.finished)
+    if(book.finished) {
+        readIcon.classList.add("read") 
+        readIcon.classList.remove("reading")
+    }
+    newBook.appendChild(readIcon)
+
+    title = document.createElement("div")
+    title.classList.add("title")
+    title.textContent = book.title
+    newBook.appendChild(title)
+
+    line1 = document.createElement("div")
+    line1.classList.add("line")
+    newBook.appendChild(line1)
+
+    author = document.createElement("div")
+    author.classList.add("author")
+    author.textContent = book.author
+    newBook.appendChild(author)
+
+    line2 = document.createElement("div")
+    line2.classList.add("line")
+    newBook.appendChild(line2)
+
+    pagesProgress = document.createElement("div");
+    const PAGES_READ = book.pagesRead,
+    TOTAL_PAGES = book.pages;
+    pagesProgress.textContent = `${PAGES_READ} / ${TOTAL_PAGES}`
+    newBook.appendChild(pagesProgress)
+
+    line3 = document.createElement("div")
+    line3.classList.add("line")
+    newBook.appendChild(line3)
+
+    percentageComplete = document.createElement("div");
+    percentageComplete.classList.add("percentage")
+    const PERCENT_COMPLETE = (PAGES_READ / TOTAL_PAGES) * 100
+    percentageComplete.textContent = `${Math.round(PERCENT_COMPLETE)} %`
+    newBook.appendChild(percentageComplete)
+
+    SHELF.replaceChild(newBook, bookIndex)
 }
 
 /* 
@@ -187,5 +262,3 @@ once saved it appears from left side
 this will spill to next row
 
 */
-
-updateDisplay(myLibrary)
